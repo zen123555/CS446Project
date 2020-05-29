58c331f96320febfb3a9d483313f34128d513901
zach-nelson <49630043+zen123555@users.noreply.github.com>
2020-05-29 13:28:15 -0700

    Add files via upload

---
+++ implementation.js
@@ -0,0 +1,24 @@
+
+/*checks that user entered valid input into the forms*/
+function validateForm() {
+  var name = document.forms["credentials"]["cName"].value;
+  var password = document.forms["credentials"]["password"].value;
+  if (name == "") {
+    alert("Please enter a valid faculty ID login");
+    return false;
+  }
+  else if(password == "") {
+  	alert("Please enter a valid password associated with your faculty login credentials");
+  	return false;
+  }
+} 
+
+/*fills in the progress bar relative to the value passed into this function 
+  Values scale: 0 (bar appears to be at 0%) - 70 (bar appears to be at 100%).  
+*/
+function updateProgress(barIncrease) {
+  var pBar = document.getElementById('p');
+  pBar.value += barIncrease;
+  pBar.getElementsByTagName('span')[0].innerHTML = Math.floor((100 / 70) * value);
+}
+

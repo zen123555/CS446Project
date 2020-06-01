964849c8409cc61537325a8a58f4294bcbf1e701
mvaldivia99 <michellvaldivia1@live.com>
2020-06-01 14:19:14 -0700

    Alerts the results

--- implementation.js
+++ implementation.js
@@ -3,15 +3,17 @@ function downloadResults() {
     // get the results pane from the DOM
     resultsPane = document.getElementById("results").getElementsByTagName("p");
 
-    // create an array each line of text inside the results pane
-    resultsList = [];
-    
+    resultsStr = "";
+
     for (i = 0; i < resultsPane.length; i++)
     {
-        alert(resultsPane[i].innerHTML);
-        resultsList.push(resultsPane[i].innerHTML);
+        resultsStr = resultsStr + resultsPane[i].innerHTML + "\n";
     }
+
+    alert(resultsStr);
     
+
+
     // This part would be reserved for writing to a file
 
     // We will not be able to fake this part, as it is not possible

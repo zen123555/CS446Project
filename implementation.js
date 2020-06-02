25ae52b650bf88ae540f7124992d3acc7f332448
mvaldivia99 <michellvaldivia1@live.com>
2020-06-01 19:56:22 -0700

    Combined Zach's changes

--- implementation.js
+++ implementation.js
@@ -1,19 +1,20 @@
 function downloadResults() {
 
-    // get the results pane from the DOM
-    resultsPane = document.getElementById("results").getElementsByTagName("p");
-
-    resultsStr = "";
-
-    for (i = 0; i < resultsPane.length; i++)
+    if (validateForm())
     {
-        resultsStr = resultsStr + resultsPane[i].innerHTML + "\n";
+        // get the results pane from the DOM
+        resultsPane = document.getElementById("results").getElementsByTagName("p");
+
+        resultsStr = "";
+
+        for (i = 0; i < resultsPane.length; i++)
+        {
+            resultsStr = resultsStr + resultsPane[i].innerHTML + "\n";
+        }
+
+        alert(resultsStr);
     }
-
-    alert(resultsStr);
     
-
-
     // This part would be reserved for writing to a file
 
     // We will not be able to fake this part, as it is not possible
@@ -26,22 +27,27 @@ function downloadResults() {
   Values scale: 0 (bar appears to be at 0%) - 70 (bar appears to be at 100%).  
 */
 function updateProgress(barIncrease) {
-  var pBar = document.getElementById('p');
-  pBar.value += barIncrease;
-  pBar.getElementsByTagName('span')[0].innerHTML = Math.floor((100 / 70) * pBvar.value);
+
+    var pBar = document.getElementById('p');
+    pBar.value += barIncrease;
+    pBar.getElementsByTagName('span')[0].innerHTML = Math.floor((100 / 70) * pBvar.value);
+
 }
 
 /*checks that user entered valid input into the forms*/
 function validateForm() {
-  var name = document.forms["credentials"]["cName"].value;
-  var password = document.forms["credentials"]["password"].value;
-  if (name == "") {
-    alert("Please enter a valid faculty ID login");
-    return false;
-  }
-  else if(password == "") {
-  	alert("Please enter a valid password associated with your faculty login credentials");
-  	return false;
-  }
+  
+    var name = document.forms["credentials"]["cName"].value;
+    var password = document.forms["credentials"]["password"].value;
+    
+    if (name == "") {
+        alert("Please enter a valid faculty ID login");
+        return false;
+    }
+    else if(password == "") {
+  	    alert("Please enter a valid password associated with your faculty login credentials");
+  	    return false;
+    }
+
 } 
 
